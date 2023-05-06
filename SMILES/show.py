76144bd835e12ab5e0d0004a41f19fe752b8b8e7
Macaulay001 <93085297+Macaulay001@users.noreply.github.com>
2023-05-06 08:11:45 +0100

    Update show.py

--- SMILES/show.py
+++ SMILES/show.py
@@ -52,3 +52,6 @@ for cell in row:
         cleaned_cell.value = cell.value
         cleaned_cell.coordinate = cleaned_cell.column_letter + str(len(non_empty_rows)+1)
     cleaned_row.append(cell.value)
+
+    
+    ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ','AK','AL','AM','AN','AO','AP','AQ','AR','AS','AT','AU','AV','AW','AX','AY','AZ']
