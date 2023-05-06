d73e0a8cc72a794660ba4cb8e75d35c067fd794a
Macaulay001 <93085297+Macaulay001@users.noreply.github.com>
2023-05-06 06:38:53 +0100

    Update show.py

--- SMILES/show.py
+++ SMILES/show.py
@@ -1,20 +1,40 @@
 import openpyxl
 
-# Load the Excel workbook
-workbook = openpyxl.load_workbook('my_workbook.xlsx')
+# Load the workbook
+workbook = openpyxl.load_workbook('example.xlsx')
 
-# Select a specific sheet by name
+# Select the sheet you want to work with
 worksheet = workbook['Sheet1']
 
-# Set the column number you want to remove empty rows from
-column_number = 1
+# Specify the column you want to delete empty rows from
+column_to_check = 'A'
 
-# Loop through all the rows in the sheet
-for row in worksheet.iter_rows():
-    # Check if the cell in the specified column is empty
-    if not row[column_number-1].value:
-        # If it is empty, delete the row
-        worksheet.delete_rows(row[0].row)
+# Keep track of the non-empty rows in the specified column
+non_empty_rows = []
 
-# Save the workbook with the changes
-workbook.save('my_workbook.xlsx')
+# Loop through all rows in the worksheet
+for row in worksheet.iter_rows(min_row=1, max_col=1, values_only=True):
+    # If the cell in the specified column is not empty, add the row to the non-empty rows list
+    if row[0]:
+        non_empty_rows.append(row)
+
+# Clear the existing contents of the worksheet
+worksheet.delete_cols(1,1)
+worksheet.append([column_to_check])
+
+# Write the non-empty rows back to the worksheet
+for row in non_empty_rows:
+    worksheet.append(row)
+
+# Create a new worksheet to store the cleaned up version of the data
+cleaned_worksheet = workbook.create_sheet('Cleaned Data')
+
+# Copy the column label to the new worksheet
+cleaned_worksheet.append([column_to_check])
+
+# Write the non-empty rows to the new worksheet
+for row in non_empty_rows:
+    cleaned_worksheet.append(row)
+
+# Save the cleaned up version of the workbook
+workbook.save('example_cleaned.xlsx')
